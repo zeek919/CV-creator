07e078acc67079c03c80bd90d37f6135f4fe0d86
Zeek918 <zeek19988@wp.pl>
2020-02-27 01:03:16 +0100

    Basic file structure and scss

---
+++ src/views/RootView/RootView.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import style from './Root.module.scss';
+import '../../style/common.css';
+
+const Root = () => <div className={style.test}>Root</div>;
+
+export default Root;
